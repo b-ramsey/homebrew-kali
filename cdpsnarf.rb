ddf64a132d85ea81157f6665c1acf1b994674b28
davidrpiper <drppublic@gmail.com>
2015-03-01 16:42:21 +1030

    cdpsnarf

--- cdpsnarf.rb
+++ cdpsnarf.rb
@@ -1,6 +1,6 @@
 class Cdpsnarf < Formula
   homepage "https://github.com/Zapotek/cdpsnarf"
-  url "https://codeload.github.com/Zapotek/cdpsnarf/tar.gz/master.tar.gz"
+  url "https://github.com/Zapotek/cdpsnarf/archive/master.tar.gz"
   version "0.6.1"
   sha1 "0ee5d3b030294e519713779c41b2c3168aeb3701"
 
