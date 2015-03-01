20972449b4f4db081ce729fb4a13f944b3a20697
davidrpiper <drppublic@gmail.com>
2015-03-01 16:04:40 +1030

    bing-ip2hosts

--- bing-ip2hosts.rb
+++ bing-ip2hosts.rb
@@ -4,6 +4,9 @@ class BingIp2hosts < Formula
   sha1 "f95b519dd107b9e4009d4c19e33304aba9e22fb6"
 
   def install
+    text = File.read("bing-ip2hosts")
+    new_contents = text.gsub(/mktemp -p \$TMPDIR/, "mktemp")
+    File.open("bing-ip2hosts", "w") { |file| file.puts new_contents }
     bin.install "bing-ip2hosts"
   end
 end
