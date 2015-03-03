3f307294c68527d5d0f578a210b70312fa72f4b6
davidrpiper <drppublic@gmail.com>
2015-03-03 15:20:11 +1030

    acccheck test

--- acccheck.rb
+++ acccheck.rb
@@ -3,35 +3,15 @@
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 
 class Acccheck < Formula
-  homepage ""
+  homepage "https://labs.portcullis.co.uk/tools/acccheck/"
+  url "https://labs.portcullis.co.uk/download/acccheck-0-2-1.tar.gz"
+  version "0.2.1"
+  sha1 "3f845b02919f7a5de6c0b24f8748be434911492c"
   head "git://git.kali.org/packages/acccheck.git"
-  
 
-  # depends_on "cmake" => :build
-  depends_on :x11 # if your formula requires any X11/XQuartz components
+  depends_on "samba"
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-
-    # Remove unrecognized options if warned by configure
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}"
-    # system "cmake", ".", *std_cmake_args
-    system "make", "install" # if this fails, try separate make/make install steps
-  end
-
-  test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! It's enough to just replace
-    # "false" with the main program this formula installs, but it'd be nice if you
-    # were more thorough. Run the test with `brew test acccheck`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+    bin.install "acccheck.pl"
   end
 end
