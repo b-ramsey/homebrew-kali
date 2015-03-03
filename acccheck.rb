40bdb5fac88d584d05c2ca27a477b4e86dd077fe
davidrpiper <drppublic@gmail.com>
2015-03-03 15:24:04 +1030

    acccheck complete

--- acccheck.rb
+++ acccheck.rb
@@ -1,7 +1,3 @@
-# Documentation: https://github.com/Homebrew/homebrew/blob/master/share/doc/homebrew/Formula-Cookbook.md
-#                /usr/local/Library/Contributions/example-formula.rb
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-
 class Acccheck < Formula
   homepage "https://labs.portcullis.co.uk/tools/acccheck/"
   url "https://labs.portcullis.co.uk/download/acccheck-0-2-1.tar.gz"
