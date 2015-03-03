2ebdb96ed7affcc628ddc721333d757c1194372c
davidrpiper <drppublic@gmail.com>
2015-03-03 14:50:27 +1030

    CDPSnarf Complete

--- cdpsnarf.rb
+++ cdpsnarf.rb
@@ -1,37 +1,12 @@
-# Documentation: https://github.com/Homebrew/homebrew/blob/master/share/doc/homebrew/Formula-Cookbook.md
-#                /usr/local/Library/Contributions/example-formula.rb
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-
 class Cdpsnarf < Formula
-  homepage ""
+  homepage "https://github.com/Zapotek/cdpsnarf"
+  url "https://github.com/Zapotek/cdpsnarf/archive/master.tar.gz"
+  version "0.6.1"
+  sha1 "0ee5d3b030294e519713779c41b2c3168aeb3701"
   head "git://git.kali.org/packages/cdpsnarf.git"
-  
-
-  # depends_on "cmake" => :build
-  depends_on :x11 # if your formula requires any X11/XQuartz components
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-
-    # Remove unrecognized options if warned by configure
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}"
-    # system "cmake", ".", *std_cmake_args
-    system "make", "install" # if this fails, try separate make/make install steps
-  end
-
-  test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! It's enough to just replace
-    # "false" with the main program this formula installs, but it'd be nice if you
-    # were more thorough. Run the test with `brew test cdpsnarf`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+    system "make", "all"
+    bin.install "cdpsnarf"
   end
 end
