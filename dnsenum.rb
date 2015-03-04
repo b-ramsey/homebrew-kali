41c660712ef1b17fc9e338788843c019579975d0
davidrpiper <drppublic@gmail.com>
2015-03-04 10:40:37 +1030

    dnsenum head-only

--- dnsenum.rb
+++ dnsenum.rb
@@ -1,37 +1,11 @@
-# Documentation: https://github.com/Homebrew/homebrew/blob/master/share/doc/homebrew/Formula-Cookbook.md
-#                /usr/local/Library/Contributions/example-formula.rb
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-
 class Dnsenum < Formula
-  homepage ""
-  head "git://git.kali.org/packages/dnsenum.git"
-  
-
-  # depends_on "cmake" => :build
-  depends_on :x11 # if your formula requires any X11/XQuartz components
+  homepage "https://github.com/fwaeytens/dnsenum.git"
+  head "https://github.com/fwaeytens/dnsenum.git"
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-
-    # Remove unrecognized options if warned by configure
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}"
-    # system "cmake", ".", *std_cmake_args
-    system "make", "install" # if this fails, try separate make/make install steps
-  end
-
-  test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! It's enough to just replace
-    # "false" with the main program this formula installs, but it'd be nice if you
-    # were more thorough. Run the test with `brew test dnsenum`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+    bin.install "dnsenum.pl"
+    (prefix/"bin").install "dns.txt"
+    prefix.install "README.md"
+    prefix.install "INSTALL.txt"
   end
 end
