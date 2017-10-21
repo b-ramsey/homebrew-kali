e844cffd1725525e8d108d254d2bb6d34400d1da
hintss <hintsss@gmail.com>
2017-10-20 22:12:57 -0700

    Use sha256 checksum for acccheck, since brew stopped allowing sha1

--- acccheck.rb
+++ acccheck.rb
@@ -2,7 +2,7 @@ class Acccheck < Formula
   homepage "https://labs.portcullis.co.uk/tools/acccheck/"
   url "https://labs.portcullis.co.uk/download/acccheck-0-2-1.tar.gz"
   version "0.2.1"
-  sha1 "3f845b02919f7a5de6c0b24f8748be434911492c"
+  sha256 "fc2274dfa16207e1b74c6fb40307286cfb4d83d3d3f842737c8e47f7c531e380"
   head "git://git.kali.org/packages/acccheck.git"
 
   depends_on "samba"
